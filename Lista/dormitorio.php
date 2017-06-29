103089c9c7aa08d4d3e82f3d8bb89a5fc4c74bbe
cetecom <cetecom@LC08L1205>
2017-06-29 13:13:52 -0400

    Listar Funcionando

--- Lista/dormitorio.php
+++ Lista/dormitorio.php
@@ -1,2 +1,18 @@
 <link href="../css/estilos.css" rel="stylesheet" type="text/css"/>
+<?php
 
+include ('../lib/conexion.php');
+
+$resultado = mysqli_query($con, "SELECT * FROM producto");
+
+
+        
+while ($registro = mysqli_fetch_array($resultado))
+{
+    echo $registro['id_producto'];
+    echo $registro['nombre_producto'];
+    echo $registro['precio'];
+    echo $registro['stock'];
+}
+
+?>
